9921f34ec188a69dfd6baf7c6a52a52ac9d23ffe
paulmillan <paul_millan@live.com.mx>
2016-04-12 10:58:57 -0600

    Added files via upload

---
+++ javascript/js/codigo.js
@@ -0,0 +1,11 @@
+	var botonClic =function()
+	{
+		
+			alert('Me dio click');
+	
+	}
+	var nombre="Martin";
+	document.write("<strong>JS en acción</strong>"+" "+nombre);
+	// pantalla modal, no te permite acceder a la pantalla de atras acapara toda la atencion
+	console.log("A punto de lanzar un mensaje")
+	alert("Mensaje por pantalla");
