160f7d62b675f7123657fc3eec7a6f6fe7d38e8e
paulmillan <paul_millan@live.com.mx>
2016-04-17 21:16:56 -0500

    calculadora

---
+++ calculadora/js/calculadora.js
@@ -0,0 +1,49 @@
+
+//variable global
+var operador="";
+function numeros(num)
+{
+
+	if(operador=="")//escribir en el operando1
+	{
+		var valor=document.calculadora.operando1.value;
+		if(valor=="0")//vaciamos la caja 
+		{
+			document.calculadora.operando1.value="";
+		}
+			
+	//concatenas los valores de num con los del operando1
+	document.calculadora.operando1.value=
+	document.calculadora.operando1.value + num;
+		
+	}
+	else
+	{
+		var valor=document.calculadora.operando2.value;
+		if(valor=="0")//vaciamos la caja 
+		{
+			document.calculadora.operando2.value="";
+		}
+			
+	//concatenas los valores de num con los del operando1
+	document.calculadora.operando2.value=
+	document.calculadora.operando2.value + num;
+		
+	}
+} 
+function ce()
+{
+	document.calculadora.operando1.value="0";
+	document.calculadora.operando2.value="0";
+	document.calculadora.resultado.value="0";
+}
+function igual()
+{
+	var valor1=document.calculadora.operando1.value;
+	var valor2=document.calculadora.operando2.value;
+	 document.calculadora.resultado.value= eval(valor1+operador+valor2);
+}
+function operadores(ope)
+{
+	operador=ope;
+}
